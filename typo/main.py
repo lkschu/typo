0a592b410b7a7865e4a2274c25a8c380253995fd
Lukas K Schumann <lukas.kilian.schumann@protonmail.com>
2022-04-01 04:01:17 +0200

    added programm states; added import from files

--- typo/main.py
+++ typo/main.py
@@ -6,13 +6,13 @@ from enum import Enum, auto
 from collections import namedtuple
 import os
 import time
+import yaml
 
 import locale
 import logging
 
 from pyfiglet import Figlet
 import ueberzug.lib.v0 as ueberzug
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -24,7 +24,6 @@ log_filehandler = logging.FileHandler(
 log_formatter = logging.Formatter(fmt=f"%(asctime)s [%(levelname)-8s] %(message)s", datefmt="[%H:%M:%S]")
 log_filehandler.setFormatter(log_formatter)
 logger.addHandler(log_filehandler)
-
 
 lorem1 = (
     "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore "
@@ -47,15 +46,14 @@ lorem2 = (
 )
 sampletext = "The red fox jumpes over the lazy dog. asjdkfl jalsdf"
 samp = "a aaaaa aaaaa aaaaaaa aaa aaaaa"
-long = (
-    "a aslkd alskdfj lkajsdlfk jalsdjf laskjdflk jasdlfkj alsdjfl asjdfl jasldfjal skjflajs dlfj asldjflas "
-    "jdflajksdlf jasldfj alsdkjfl asjdflj aslkdjflaks jdlfj alsdjf jasldjf laksjdl 1234567"
-)
+long = "a aslkd alskdfj lkajsdlfk jalsdjf laskjdflk jasdlfkj alsdjfl asjdfl jasldfjal skjflajs dlfj asldjflas " \
+       "jdflajksdlf jasldfj alsdkjfl asjdflj aslkdjflaks jdlfj alsdjf jasldjf laksjdl 1234567"
 
-texts = [sampletext, lorem2]
-texts = [lorem1, samp]
-# texts = [long]
-# texts = [samp]
+
+texts1 = [sampletext, lorem2]
+texts2 = [lorem1, samp]
+texts3 = [long]
+texts4 = [samp]
 
 S_SPACE = "_"
 S_RETURN = "⏎"
@@ -72,22 +70,18 @@ def textlst(txtstr: str, width: int):
     for i, word in enumerate(tmp_lst):
 
         if len(word) > width or (len(word) + 1 > width and len(tmp_lst) > 1):
-            # this word (and 1 space) doesn't fit at all
-            # if there are at least 2 words there must be place for 1 space
+            # this word (and 1 space) doesn't fit at all if there are at least 2 words there must be place for 1 space
             raise ValueError(f"Can't fit <{word}> (plus possible space) in a width of {width}!")
 
         # if it's the last word we need no space at the end
         # +1 for space after word
         if (len(current_lst) + len(word) + 1 > width and i + 1 != len(tmp_lst)) or (
-            len(current_lst) + len(word) > width and i + 1 == len(tmp_lst)
+                len(current_lst) + len(word) > width and i + 1 == len(tmp_lst)
         ):
             # line + word to long -> new line
             ret_lst.append(current_lst)
             current_lst = []
 
-        # if len(current_lst) != 0:
-        #     # add spaces between words
-        #     current_lst.extend(" ")
         current_lst.extend(word)
         if tmp_lst[-1] != word:
             # add spaces between words and the last word aswell
@@ -122,11 +116,15 @@ class MainScreen:
         self.texts = txtlst
         # TODO maybe give typed lists for sections like this [[chars from sec1], [chars from sec2]]
         self.typed = []
-        self.typed_sum = 0  # save carrie over when new section starts
+        self.typed_sum = 0  # save carry over when new section starts
         self.err_lst = []
         self.start = time.time()
+
+        self.choice = 0  # current choice when in option menu mode
+        self.scroll = 0
+
         # Windows
-        self.textwin = None
+        self.textwin = None  # main window in center with box
         self.wpmwin = None
         self.errwin = None
         self.setsize()
@@ -137,13 +135,102 @@ class MainScreen:
     def maxx(self) -> int:
         """Max width for text, so decoration is not included"""
         maxx = self.scr.getmaxyx()[1]
-        # -1 so len(range(-0-,maxx) = max -2
         return maxx - 2
 
     @property
+    def maxy(self) -> int:
+        """Max height for text, so decoration is not included"""
+        maxy = self.scr.getmaxyx()[0]
+        return maxy - 2
+
+    @property
     def errors(self) -> int:
-        """Gives sum of errors"""
+        """ Gives sum of errors """
         return len(self.err_lst)
+
+    def make_menu(self, option_lst: list[str], current: int = 0) -> int:
+        """
+        makes a menu out of a list of options, centers on given index.
+        return index number or -1 if exit
+        """
+
+        # IDEA:
+        # ^^^   <- marks the first line in window if there are earlier lines
+        # 3. asldkfj
+        # 4. asdflasjdf
+        # vvv   <- there are more options below
+
+        oldmodus = self.modus
+        self.modus = Modus.MENU
+        curses.curs_set(0)
+
+        # shape tmp_lst
+        def shape(option_list):
+            # add numbers to each option
+            tmp_lst = [f"{i}.) {line}\n" for i, line in enumerate(option_list)]
+            # trim to visible part
+            tmp_lst = tmp_lst[self.scroll:min(self.scroll+self.maxy, self.scroll+len(tmp_lst))]
+            # Trim lines that are too long, padd lines that are too short
+            for i, line in enumerate(tmp_lst):
+                if len(line) > self.maxx + 1:
+                    line = f"{line[:self.maxx - 4]}...\n"
+                    tmp_lst[i] = line
+                # not neccessarily needed
+                elif len(line) < self.maxx + 1:
+                    line = f"{line[:-1]:<{self.maxx}}\n"
+                    tmp_lst[i] = line
+            # Add ^ / v marker
+            if self.scroll > 0:
+                tmp_lst[0] = f"{' ^^^':<{self.maxx}}\n"
+            if len(option_lst)-self.scroll > len(tmp_lst):
+                tmp_lst[-1] = f"{' vvv':<{self.maxx}}\n"
+            self.text = "".join(tmp_lst)[:-1]
+
+        shape(option_lst)
+        self.setsize()
+
+        assert isinstance(self.textwin, curses.window)
+
+        while True:
+            inp_char = self.textwin.get_wch()
+            shape(option_lst)
+
+            # inp_char is int if function key, else unicode
+            inp_key = ord(inp_char) if isinstance(inp_char, str) else inp_char
+            logger.info(f"Got keycode {inp_key}, {inp_char}")
+
+            if inp_key == curses.KEY_RESIZE:
+                # redraw screen
+                self.setsize()
+                self.draw()
+            elif inp_key == curses.KEY_UP:
+                if self.choice > 0:
+                    self.choice -= 1
+                if self.choice == 0 and self.scroll > 0:
+                    self.scroll -= 1
+                self.setsize()
+                self.draw()
+            elif inp_key == curses.KEY_DOWN:
+                if self.choice < len(self.text.split('\n')) - 1:
+                    self.choice += 1
+                if self.choice == len(self.text.split('\n')) -1 and len(option_lst)-self.scroll > len(self.text.split('\n')):
+                    self.scroll +=1
+                self.setsize()
+                self.draw()
+            elif inp_key in [10, curses.KEY_RIGHT]:
+                # enter
+                break
+            elif inp_key in [27, curses.KEY_LEFT]:
+                self.choice = -1
+                # ESC key
+                break
+
+        curses.curs_set(2)
+        self.modus = oldmodus
+        self.text = ""
+        ret = self.choice + self.scroll
+        logger.info(f"Exiting menu with {option_lst[self.choice+self.scroll]}")
+        return self.choice + self.scroll
 
     def wpm(self) -> float:
         """Get current wpm by len of self.typed and time since self.start"""
@@ -187,7 +274,7 @@ class MainScreen:
             # Handle text in 'display' mode. So normal strings, including \n can be printed
             assert isinstance(self.text, str)
             toprint = self.text.split("\n")
-            texty = len(toprint)
+            texty = min(len(toprint), self.maxy)
             textx = min(max(len(line) for line in toprint), self.maxx)
             self.textwin_xy = self._win_xy(
                 nlines=texty + 2,  # extra space for border
@@ -196,13 +283,12 @@ class MainScreen:
                 begin_x=(self.maxx - textx) // 2,
             )
 
-        logger.debug(f"Textlst:{textlst(self.text, self.maxx)}")
-        logger.debug(f"self.maxx:{self.maxx}, textx:{textx}, texty:{texty} textwin_xy = {self.textwin_xy}")
-
         self.textwin = self.scr.subwin(*self.textwin_xy)
         self.textwin.keypad(True)  # allow capturing esc sequences!
 
         if self.modus == Modus.SESSION:
+            logger.debug(f"Textlst:{textlst(self.text, self.maxx)}")
+            logger.debug(f"self.maxx:{self.maxx}, textx:{textx}, texty:{texty} textwin_xy = {self.textwin_xy}")
             self.wpmwin_xy = self._win_xy(
                 nlines=3,
                 ncols=9,
@@ -214,8 +300,8 @@ class MainScreen:
                 ncols=10,
                 begin_y=self.wpmwin_xy.begin_y,
                 begin_x=self.textwin_xy.begin_x
-                + self.textwin_xy.ncols
-                - (10 + 3),  # self.wpmwin_xy.begin_x+self.wpmwin_xy.ncols+2
+                        + self.textwin_xy.ncols
+                        - (10 + 3),  # self.wpmwin_xy.begin_x+self.wpmwin_xy.ncols+2
             )
             self.wpmwin = self.scr.subwin(*self.wpmwin_xy)
             self.wpmwin.box()
@@ -230,15 +316,13 @@ class MainScreen:
     def draw(self):
         assert isinstance(self.textwin, curses.window)
         assert isinstance(self.wpmwin, curses.window)
-        # y, x = self.textwin.getyx()
         # origin is 1,1 (because of border)
 
-        # txtlst = textlst(self.text, self.textwin_xy.ncols)
-        txtlst = textlst(self.text, self.maxx)
-        for i, line in enumerate(txtlst):
-            self.textwin.addstr(i + 1, 1, "".join(line))
-
         if self.modus == Modus.SESSION:
+            txtlst = textlst(self.text, self.maxx)
+            for i, line in enumerate(txtlst):
+                self.textwin.addstr(i + 1, 1, "".join(line))
+
             self.wpmwin.addstr(1, 1, f"WPM:{int(self.wpm()):>3}", curses.A_BOLD)
             self.wpmwin.noutrefresh()  # mark for refresh
             self.errwin.addstr(1, 1, f"ERR:{self.err():>3}%", curses.A_BOLD)
@@ -270,22 +354,54 @@ class MainScreen:
             assert isinstance(self.text, str)
 
             for i, line in enumerate(self.text.split("\n")):
-                self.textwin.addnstr(i + 1, 1, line, self.maxx)
+                if self.modus == Modus.MENU and i == self.choice:
+                    self.textwin.addnstr(i + 1, 1, line, self.maxx, curses.A_REVERSE)
+                else:
+                    self.textwin.addnstr(i + 1, 1, line, self.maxx)
 
             self.textwin.redrawwin()
             self.textwin.refresh()
         return
 
-    @ueberzug.Canvas()
-    def run(self, canvas):
-        assert isinstance(self.textwin, curses.window)
-        logger.info(f"starting run")
+    def run(self):
+        # Test menu
+        curses.curs_set(0)
+        path = "./res"
+        menu = [x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))]
+        x = self.make_menu(menu)
+        logger.info(f"Menu result: {x}, {menu}")
+        with open(os.path.join(path, menu[x]), 'r') as f:
+            s = yaml.safe_load(f)
+            self.texts = s['session']
+        self.make_session()
+        self.make_summary()
 
+    def make_summary(self):
+        self.modus = Modus.SUMMARY
+        curses.flushinp()
+        self.text = (
+            f"Typed {self.typed_sum} characters in {time.time()-self.start:.2f} sec."
+            f"\nWPM: {self.wpm():.2f}"
+            f"\nTyping errors: {self.err():>3}%."
+            f"\nAll errors: {self.err_lst}"
+        )
+        logger.info(self.text)
+        self.setsize()
+        self.textwin.get_wch()
+
+    def make_session(self):
+        assert isinstance(self.textwin, curses.window)
+        logger.info(f"starting session")
+
+        self.scr.clear()
+        self.scr.refresh()
+
+        curses.curs_set(0)
         f = Figlet(font="basic")
-        warnwin = self.scr.subwin(9, 9, 2, self.maxx // 2 - 4)
-        warnwin.addstr(0, 0, f.renderText("3"))
-        warnwin.refresh()
-        time.sleep(1)
+        # warnwin = self.scr.subwin(9, 9, 2, self.maxx // 2 - 4)
+        # warnwin.addstr(0, 0, f.renderText("3"))
+        # warnwin.refresh()
+        # time.sleep(1)
         warnwin = self.scr.subwin(9, 9, 2, self.maxx // 2 - 4)
         warnwin.addstr(0, 0, f.renderText("2"))
         warnwin.refresh()
@@ -295,6 +411,7 @@ class MainScreen:
         warnwin.refresh()
         time.sleep(1)
         self.start = time.time()
+        curses.curs_set(2)
 
         for section in self.texts:
             self.text = section
@@ -326,7 +443,7 @@ class MainScreen:
                         getmouse = curses.getmouse()
                     except curses.error:
                         getmouse = None
-                    logger.debug(f"Got mouse event inp_char,inp_key{inp_char,inp_key}, getmouse: {getmouse}")
+                    logger.debug(f"Got mouse event inp_char,inp_key{inp_char, inp_key}, getmouse: {getmouse}")
                 elif inp_key in [
                     curses.KEY_UP,
                     curses.KEY_DOWN,
@@ -339,7 +456,7 @@ class MainScreen:
                     pass
                 elif inp_key == 27:
                     # ESC key
-                    logger.debug(f"Got esc event inp_char,inp_key{inp_char,inp_key},{curses.ungetch(inp_char)}")
+                    logger.debug(f"Got esc event inp_char,inp_key{inp_char, inp_key},{curses.ungetch(inp_char)}")
                     break
                 elif inp_key == curses.KEY_BACKSPACE or inp_key == 127 or str(inp_char) == "^?":
                     # elif inp_key in [curses.KEY_BACKSPACE, '\b', '\x7f']:
@@ -403,15 +520,6 @@ class MainScreen:
 
             logger.info(f"At end while loop")
         logger.info(f"Exiting while loop")
-        self.modus = Modus.SUMMARY
-        curses.flushinp()
-        self.text = (
-            f"Typed {self.typed_sum} characters in {self.start} sec."
-            f"\nTyping errors: {self.err():>3}%."
-            f"\nAll errors: {self.err_lst}"
-        )
-        self.setsize()
-        time.sleep(10)
 
 
 #
@@ -433,8 +541,8 @@ def main():
         curses.start_color()
         curses.mousemask(curses.BUTTON1_CLICKED)
         stdscr.keypad(True)
-        logger.info(f"stdscr is window? {isinstance(stdscr,curses.window)}")
-        myscr = MainScreen(stdscr, texts)
+        logger.info(f"stdscr is window? {isinstance(stdscr, curses.window)}")
+        myscr = MainScreen(stdscr, ['x'])
         myscr.run()
     finally:
         if stdscr is not None:
@@ -466,6 +574,5 @@ if __name__ == "__main__":
 
     test_textlist()
     print(f"all tested")
-    time.sleep(1)
 
     main()
